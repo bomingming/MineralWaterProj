28a760f8f53bb86f56e3e64a3f6cb525a3cd6fd7
bom <bomingming>
2023-01-05 16:52:40 +0900

    지역 선택 radionButton 정렬

--- app/src/main/java/kr/co/company/mineralwater/LocDialogFragment.java
+++ app/src/main/java/kr/co/company/mineralwater/LocDialogFragment.java
@@ -1,14 +1,29 @@
 package kr.co.company.mineralwater;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.Nullable;
+
 public class LocDialogFragment extends DialogFragment {
+    /*@NonNull
+    @Override
+    public Dialog onCreateDilaog(@Nullable Bundle savedInstanceSate){
+        CharSequence[] items = {"선택 안 함","경기도","강원도","경상도"};
+        boolean[] checkedItem = {true, false, false, false};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setPositiveButton("선택",null).setCancelable(false);
+    }*/
+
     private Fragment fragment;
 
     public LocDialogFragment(){
@@ -23,6 +38,14 @@ public class LocDialogFragment extends DialogFragment {
 
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        /*int width = getResources().getDimensionPixelSize(R.dimen.popup_width);
+        int height = getResources().getDimensionPixelSize(R.dimen.popup_height);
+        getDialog().getWindow().setLayout(width, height);*/
     }
 
 }
