157403c8ad9c19a4af007b59c9d8cbb9c927d8a5
bomingming <byj060362@gmail.com>
2023-01-25 12:12:58 +0900

    인터넷 연결 여부 확인 기능 구현

--- app/src/main/java/kr/co/company/mineralwater/LodingActivity.java
+++ app/src/main/java/kr/co/company/mineralwater/LodingActivity.java
@@ -1,15 +1,54 @@
 package kr.co.company.mineralwater;
 
+import android.accessibilityservice.AccessibilityService;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 public class LodingActivity extends Activity {
+    AlertDialog alertDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loaing_main);
-        startLoading();
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean isNetwork = false;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            Network network = connectivityManager.getActiveNetwork();
+            if(network != null){
+                NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+                if(capabilities != null){
+                    isNetwork = true;
+                }
+            }
+        }
+        if(isNetwork == false){ // 인터넷에 연결되지 않은 경우
+            // 다이얼로그 생성
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+            builder.setTitle("종료");
+            builder.setMessage("인터넷 연결이 원활하지 않아 어플이 종료됩니다.");
+            builder.setPositiveButton("확인", null);
+            alertDialog = builder.create();
+            builder.show();
+        }else if(isNetwork == true){ // 인터넷에 연결된 경우
+            startLoading(); // 로딩 후 화면 전환
+        }
+
     }
 
     private void startLoading() {
